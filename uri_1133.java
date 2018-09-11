be426fb672e012c10b75aeca9d566c7550403e0a
Mehadi Hassan <mail@mehadihassan.com>
2018-09-11 20:04:10 +0600

    URI 1133

---
+++ uri_1133.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class uri_1133{
+  public static void main (String [] args){
+    
+    Scanner sc  = new Scanner (System.in);
+    
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    
+    if(num1>num2){
+      int temp = num2;
+      num2 = num1;
+      num1 = temp;
+    } 
+    
+    for(int i = num1+1 ; i<num2 ; i++){
+      int temp = i%5;
+      if(temp == 2 || temp == 3){
+        System.out.println(i);
+      }
+      
+    } 
+    
+  }
+}
